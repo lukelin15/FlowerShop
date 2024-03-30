61b38a8a468f68160c3b2aedafad44b7b361d307
Peolline <larasati.gadis.erm@gmail.com>
2024-03-31 00:03:33 +0700

    bunch of stuff

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -5,9 +5,22 @@
 <main>
   <h1>Svelte template</h1>
 
-  <p>Write your HTML here</p>
+  <p><b>HERE!!!!!!!!!!!!!!!!!!!!!</b>Write your HTML here</p>
+
+  <section>
+    <div class="Top">
+      <p> Here </p>
+
+    </div>
+  </section>
 </main>
 
 <style>
-  /* Write your CSS here */
+  /* ####################################################Write your CSS here############################################################# */
+
+  .Top{
+    color: rgb(0, 136, 255);
+  }
+
 </style>
+
