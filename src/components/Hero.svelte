cb81624365097cb53e705c6c749aab15172d4334
Peolline <larasati.gadis.erm@gmail.com>
2024-03-31 20:04:46 +0700

    sidebar shorty

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,7 +1,8 @@
 <div class="hero">
   <div class="banner">
     <h1><b>Blossom Your Moments</b></h1>
-    <p>Embrace your choices - we do</p>
+    <h2>Natural & Fresh Flowers</h2>
+    <p>Embrace your choices - we do. Here are the description small thing about this shop. spam sssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
     <slot />
   </div>
 </div>
