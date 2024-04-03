37e26ad9a5bc4751b105b9a847aa485d7a0d37de
Peolline <larasati.gadis.erm@gmail.com>
2024-04-03 10:58:14 +0700

    lets see

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,7 +2,7 @@
   <div class="banner">
     <h1><b>Blossom Your Moments</b></h1>
     <h2>Natural & Fresh Flowers</h2>
-    <p>Embrace your choices - we do. Here are the description small thing about this shop. spam sssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+    <p>Embrace your choices - we do. Here are the description small thing about this shop. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. Write ur description here. </p>
     <slot />
   </div>
 </div>
